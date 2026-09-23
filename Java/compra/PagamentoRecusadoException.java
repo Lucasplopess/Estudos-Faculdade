01a274c9c1b094be12f67e7ce37ae2f1a7888ced
Lucas de Paula <lucaspchaves2007@gmail.com>
2026-09-23 16:15:59 -0300

    Add files via upload

---
+++ Java/compra/PagamentoRecusadoException.java
@@ -0,0 +1,8 @@
+package compra;
+
+public class PagamentoRecusadoException extends Exception{
+    
+    public PagamentoRecusadoException(String mensagem) {
+        super(mensagem);
+    }
+}
---
+++ Java/compra/PagamentoService.java
@@ -0,0 +1,13 @@
+package compra;
+
+public class PagamentoService {
+    public void pagar (double valor)
+            throws PagamentoRecusadoException {
+        if (valor > 3000){
+            throw new PagamentoRecusadoException(
+                "Pagamento recusado pela operadora."
+            );
+        }
+        System.out.println("Pagamento de R$ " + valor + " aprovado.");
+    }
+}
